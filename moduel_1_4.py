b994214f0e3e14fb0e23da43577382ef628c48a5
yo1ch1 <dmit.petukhov2017@gmail.com>
2024-08-22 23:36:39 +1200

    Add files via upload

---
+++ moduel_1_4.py
@@ -0,0 +1,9 @@
+#Программа
+my_string = input('Введите слово: ')
+print('Слово: ', my_string),
+print('Колличесвто символов: ', len(my_string))
+#Работа с методами строк
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[-1])
